ba3c53f9b96030b83f6a91f31ff3b2c55f8f0242
Pranav Rangate. <pranavrangate231104@gmail.com>
2025-07-07 18:28:44 +0530

    Add files via upload

---
+++ star_patterns/rev_cen_pyramid.java
@@ -0,0 +1,17 @@
+public class rev_cen_pyramid {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = n; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
