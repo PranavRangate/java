c645a65ff2cb740f1ecc36249374e8164cba98a4
Pranav Rangate. <pranavrangate231104@gmail.com>
2025-07-07 18:18:08 +0530

    central pyramid

---
+++ star_patterns/piramid.java
@@ -0,0 +1,17 @@
+public class piramid {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
