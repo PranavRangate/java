59b379d552df513dbd03aab81fb1715197c2290c
Pranav Rangate. <pranavrangate231104@gmail.com>
2025-07-08 20:40:05 +0530

    Add files via upload

---
+++ star_patterns/diamond.java
@@ -0,0 +1,30 @@
+public class diamond {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
