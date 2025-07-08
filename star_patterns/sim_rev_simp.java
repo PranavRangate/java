25fc22ca7bfc2c3645cc188dee1adb13b757214f
Pranav Rangate. <pranavrangate231104@gmail.com>
2025-07-08 20:39:39 +0530

    Add files via upload

---
+++ star_patterns/sim_rev_simp.java
@@ -0,0 +1,16 @@
+public class sim_rev_simp {
+    public static void main(String args[]) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 4; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
