262d18c863b8108d8d26e35178399808eaf520ef
Pranav Rangate. <pranavrangate231104@gmail.com>
2025-07-09 19:55:55 +0530

    Create simple.java

---
+++ Array_questions/simple.java
@@ -0,0 +1,12 @@
+public class simple {
+    /**
+     * This is a simple Java program that initializes an array of integers
+     * and prints each element to the console.
+     */
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
