ceca241601a9f7b9a7e08530dfab5d955a8e152e
Pranav Rangate. <pranavrangate231104@gmail.com>
2025-07-14 23:52:05 +0530

    Create simple.java

---
+++ String_questions/simple.java
@@ -0,0 +1,16 @@
+public class simple{
+    public static void main(String[] args) {
+        String f_name = "Pranav";
+        System.out.println(f_name);
+
+        String s_name = "Ranagte";
+        System.out.println(s_name);
+
+        String name = f_name.concat(" ").concat(s_name);
+        System.out.println(name);
+
+        String full_name = f_name + " " + s_name;
+        System.out.println(full_name);
+
+    }
+}
